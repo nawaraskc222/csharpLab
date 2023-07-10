db72e2f7356ed1e3762b4f1685011debe8d31def
Nawaras kc <nawaraskc888@gmail.com>
2023-07-10 06:48:09 +0545

    file handling

---
+++ FileHandling/FileHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\nawar\Downloads\\demo.txt";
+            string data = "im KCT";
+            File.WriteAllText(path, data);
+
+            string appendData = " im studnet of KCT";
+            File.AppendAllText(path, appendData);
+
+            string readFile= File.ReadAllText(path);
+            Console.WriteLine(readFile);
+           
+        }
+    }
+}
