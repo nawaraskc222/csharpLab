45e9a155b2b0908588eee49f765a0a2095eb23ac
Nawaras kc <nawaraskc888@gmail.com>
2023-07-11 15:34:53 +0545

    lamda

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -18,23 +18,22 @@ namespace LINQ
         static void Main(string[] args)
         {
 
-            // Create a list of students
+          
             List<Student> students = new List<Student>();
 
-            // Add details of five students
+          
             students.Add(new Student { FirstName = "Saksham", LastName = "Khanal", Address = "Lalitpur", College = "KCT" });
             students.Add(new Student { FirstName = "Bibke", LastName = "Karki", Address = "Kathmandu", College = "KCT" });
             students.Add(new Student { FirstName = "Roshan", LastName = "sth", Address = "Lalitpur", College = "BKT campus" });
             students.Add(new Student { FirstName = "Niusm", LastName = "Giri", Address = "Lalitpur", College = "KCT" });
             students.Add(new Student { FirstName = "Sita", LastName = "Karki", Address = "Bhaktapur", College = "GGIC" });
 
-            // LINQ query to filter records and order by first name descending
+            
             var filteredStudents = from student in students
                                    where student.Address == "Lalitpur" && student.College == "KCT"
                                    orderby student.FirstName descending
                                    select student;
 
-            // Print the filtered and ordered students' details
             Console.WriteLine("Students with Address 'Lalitpur' and College 'KCT' (Ordered by First Name Descending):");
             foreach (var student in filteredStudents)
             {
---
+++ Lamda/Lamda/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+          
+            var evenNumbers = numbers.FindAll(num => num % 2 == 0);
+
+           
+            var squares = numbers.ConvertAll(num => num * num);
+
+          
+            Console.WriteLine("Even Numbers:");
+            foreach (var num in evenNumbers)
+            {
+                Console.WriteLine(num);
+            }
+
+           
+            Console.WriteLine("Squares of Numbers:");
+            foreach (var square in squares)
+            {
+                Console.WriteLine(square);
+            }
+        }
+    }
+}
