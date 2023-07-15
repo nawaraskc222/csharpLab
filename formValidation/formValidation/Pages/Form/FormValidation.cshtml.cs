205dd3916fc4e8729d9aad286750185e943519b4
Nawaras kc <nawaraskc888@gmail.com>
2023-07-15 07:11:48 +0545

    form validation using jquery

---
+++ formValidation/formValidation/Pages/Form/FormValidation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace formValidation.Pages.Form
+{
+    public class FormValidationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
