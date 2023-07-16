6694ee651d3ccb8e84fdd44e2d76670e97c21f70
Nawaras kc <nawaraskc888@gmail.com>
2023-07-16 07:49:32 +0545

    fixed cookie

--- Cookies/Cookies/Pages/CookiesWrite.cshtml.cs
+++ Cookies/Cookies/Pages/CookiesWrite.cshtml.cs
@@ -8,12 +8,19 @@ namespace Cookies.Pages
         
 
         [BindProperty]
-        public string CookiesName { get; set; }
+        public string email { get; set; }
+        [BindProperty]
+        public string firstname { get; set; }
+        [BindProperty]
+        public string lastname { get; set; }
         public async Task<IActionResult> OnPostAsync()
         {
             CookieOptions option = new CookieOptions();
             option.Expires = DateTimeOffset.Now.AddMinutes(5);
-            Response.Cookies.Append("myName", CookiesName, option);
+            Response.Cookies.Append("firstname", firstname, option);
+            Response.Cookies.Append("lastname", lastname, option);
+           
+            Response.Cookies.Append("email", email, option);
 
             return RedirectToPage("./DisplayCookie");
         }
--- Cookies/Cookies/Pages/DisplayCookie.cshtml.cs
+++ Cookies/Cookies/Pages/DisplayCookie.cshtml.cs
@@ -5,10 +5,14 @@ namespace Cookies.Pages
 {
     public class DisplayCookieModel : PageModel
     {
-        public string CookieName;
+        public string firstname;
+        public string lastname;
+        public string email;
         public void OnGet()
         {
-            CookieName = Request.Cookies["myName"];
+            firstname = Request.Cookies["firstname"];
+            lastname = Request.Cookies["lastname"];
+            email = Request.Cookies["email"];
         }
     }
 }
